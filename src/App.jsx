9226abcb95d40a7623f724f7ad00d0bc1adde63b
Patrick OM <64801567+patricko5@users.noreply.github.com>
2022-09-13 23:26:30 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import Employees from './Employees';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Employees />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
